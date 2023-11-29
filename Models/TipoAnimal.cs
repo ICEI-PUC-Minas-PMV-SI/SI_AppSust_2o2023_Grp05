805c964e6878e41ba1779106f73d25496ae502d6
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2023-11-29 09:07:35 +0100

    botao salvar funcionando

---
+++ Models/TipoAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetFriends.Models
+{
+    public class TipoAnimal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Tipo animal obrigatório")]
+        [MaxLength(50)]
+        public String TipoDesc { get; set; }
+    }
+}
