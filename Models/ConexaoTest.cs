f1d3951a735d47bf485a50c88a97229c0f1415e1
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2023-12-02 00:46:42 +0100

    removendo waist da minha versão

--- Models/ConexaoTest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PetFriends.Models
-{
-    public class ConexaoTest
-    {
-        public int id { get; set; }
-        public string nome { get; set; }
-        public string notes { get; set; }
-    }
-}
