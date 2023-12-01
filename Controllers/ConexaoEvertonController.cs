f89e8703f11f8d2f6d1ba678e0c111e97e81fc65
Everton <Everton@DESKTOP-03BECVT>
2023-12-01 11:11:06 -0300

    Primeiro commit

---
+++ Controllers/ConexaoEvertonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetFriends.Data;
+
+namespace PetFriends.Controllers
+{
+    public class ConexaoEvertonController : Controller
+    {
+        private readonly ApplicationContext _context;
+        public ConexaoEvertonController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public IActionResult ConexaoEverton()
+        {
+            ViewBag.users = _context.ConexaoEverton.ToList();
+            return View();
+        }
+    }
+}
---
+++ Models/ConexaoEverton.cs
@@ -0,0 +1,9 @@
+﻿namespace PetFriends.Models
+{
+    public class ConexaoEverton
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Notes { get; set; }
+    }
+}
