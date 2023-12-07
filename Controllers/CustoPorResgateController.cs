3d1f192fe481a3967c678700435c3fab9c2fc017
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2023-12-07 19:01:54 +0100

    Custo por resgate - CRUD Completo funciona - falta ajustar o design

--- Controllers/CustoPorResgateController.cs
+++ Controllers/CustoPorResgateController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PetFriends.Data;
+using PetFriends.Models;
+using System.Web;
 
 namespace PetFriends.Controllers
 {
@@ -10,10 +12,72 @@ namespace PetFriends.Controllers
         {
             _context = context;
         }
-        public IActionResult CustoPorResgate()
+
+        public ActionResult Index()
         {
-            ViewBag.users = _context.CustoPorResgate.ToList();
-            return View();
+            return View(_context.CustoPorResgate.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            CustoPorResgate CustoPorResgate = new CustoPorResgate();
+
+            ViewBag.Acao = CustoPorResgate.Acao;
+            ViewBag.CustoMacho = CustoPorResgate.CustoMacho;
+            ViewBag.CustoFemea = CustoPorResgate.CustoFemea;
+            ViewBag.Ativo = CustoPorResgate.Ativo;
+            ViewBag.DataCadastro = CustoPorResgate.DataCadastro;
+
+            return View(CustoPorResgate);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CustoPorResgate custoPorResgate)
+        {
+            _context.Add(custoPorResgate);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var custoPorResgate = _context.CustoPorResgate.Where(d => d.Id == id).FirstOrDefault();
+
+            return View(custoPorResgate);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CustoPorResgate Model)
+        {
+            var custoPorResgate = _context.CustoPorResgate.Where(d => d.Id == Model.Id).FirstOrDefault();
+
+            if (custoPorResgate != null)
+            {
+                custoPorResgate.Acao = Model.Acao;
+                custoPorResgate.CustoMacho = Model.CustoMacho;
+                custoPorResgate.CustoFemea = Model.CustoFemea;
+                custoPorResgate.Ativo = Model.Ativo;
+                custoPorResgate.DataCadastro = Model.DataCadastro;
+
+                _context.SaveChanges();
+
+                // It will redirect to the Read method
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        // POST: /Animal/Delete/id/
+        public ActionResult Delete(int id)
+        {
+            var custoPorResgate = _context.CustoPorResgate.Where(d => d.Id == id).FirstOrDefault();
+            _context.CustoPorResgate.Remove(custoPorResgate);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/CustoPorResgate.cs
+++ Models/CustoPorResgate.cs
@@ -1,12 +1,40 @@
-﻿namespace PetFriends.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetFriends.Models
 {
     public class CustoPorResgate
     {
+        [Key]
         public int Id { get; set; }
-        public string? Acao { get; set; }
-        public double? CustoMacho { get; set; }
-        public double? CustoFemea{ get; set; }
+
+        [Display(Name = "Ação")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "* Ação é obrigatório")]
+        [MaxLength(100)]
+        [StringLength(100)]
+        public String Acao { get; set; }
+
+        [Display(Name = "Custo Macho")]
+        [Required(ErrorMessage = "Custo Macho é obrigatório")]
+        [Range(0, int.MaxValue, ErrorMessage = "Custo somente acima de 0.")]
+        public double CustoMacho { get; set; }
+
+        [Display(Name = "Custo Fêmea")]
+        [Required(ErrorMessage = "Custo Fêmea é obrigatório")]
+        [Range(0, int.MaxValue, ErrorMessage = "Custo somente acima de 0.")]
+        public double CustoFemea{ get; set; }
+
+        [Display(Name = "Data do Cadastro")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Data do Cadastro do pet é obrigatório")]
+        [MaxLength(10)]
+        [StringLength(10)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public String DataCadastro { get; set; }
+
+        [Display(Name = "Ativo: Sim-Não")]
+        [Required(ErrorMessage = "Campo obrigatório")]
         public bool Ativo { get; set; }
-        public DateTime DataCadastro { get; set; }
+        
     }
 }
