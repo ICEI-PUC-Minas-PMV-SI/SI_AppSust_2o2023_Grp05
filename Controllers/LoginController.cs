714adb17eeae5aea5fa467d0f41f6c2d441b4d64
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2023-12-10 17:20:15 +0100

    Home + tela de login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetFriends.Data;
+using PetFriends.Models;
+
+namespace PetFriends.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ApplicationContext _context;
+        public LoginController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login login)
+        {
+            //if (ModelState.IsValid)
+            //{
+                var l = _context.Usuario.Where(l => l.Email.Equals(login.Email) && l.Senha.Equals(login.Senha) && l.IdTipoUsuario.Equals(1)).FirstOrDefault();
+                    if (l != null)
+                    {
+                        //Session["log"] = l.Email.ToString();
+                        //return RedirectToAction("~/Home");
+                        return Redirect(Url.Action("Index", "HomeAfterLogin"));
+                    }
+
+            //}
+
+            return Redirect(Url.Action("Index", "Home"));
+            //return View("~/Home");
+        }
+    }
+}
+
+
+
+
---
+++ Models/Login.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetFriends.Models
+{
+    public class Login
+    {
+        [Display(Name = "Email")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "* Email é obrigatório", AllowEmptyStrings = false)]
+        [MinLength(10)]
+        [MaxLength(100)]
+        public String Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "* Senha é obrigatória", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        [MinLength(10)]
+        [MaxLength(100)]
+        public String Senha { get; set; }
+                
+    }
+}
+
+
