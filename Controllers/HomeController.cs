d029a1645f7c6a8c24f9fc0abac422f8a013fddc
Sara Vidal <36409243+Saracvidal@users.noreply.github.com>
2023-12-12 23:18:02 +0100

    ajeitando o design para alguns detalhers em todas as telas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using PetFriends.Data;
 using PetFriends.Models;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
 
 namespace PetFriends.Controllers
 {
@@ -22,6 +24,36 @@ namespace PetFriends.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Email(Email email)
+        {
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            MailMessage mailMessage = new MailMessage();
+            smtpClient.Credentials = new NetworkCredential
+            {
+                UserName = "ongpetfriends@gmail.com",
+                Password = "Ogn2023pet2023friends!@#$"
+
+                //"ucwn etap ghsg oqjg"
+                //turn on 2-factor authentication first
+                //create app pass: https://myaccount.google.com/u/0/apppasswords?pli=1&rapt=AEjHL4NuuWD9E7vyqwfN8hCnd1eNF8qX4l7p_lugZgr8hiICebARA5VqSXgkT2b38gnX72MVEj1d98dkTPC93reaaXIy5Bh9lqSDRfWrrADSJzV95NwucE0
+            };
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            mailMessage.From = new MailAddress(email.Remetente);
+            mailMessage.To.Add("ongpetfriends@gmail.com");
+            mailMessage.Subject = "Novo Formulário de Doaçao " + email.NomeRemetente;
+            mailMessage.Body = string.Format("{0} {1} {2}", email.NomeRemetente, email.EnderecoRemetente, email.Mensagem);
+
+            if (smtpClient != null)
+                smtpClient.Send(mailMessage);
+
+            return View();
         }
     }
 }
---
+++ Models/Email.cs
@@ -0,0 +1,13 @@
+﻿namespace PetFriends.Models
+{
+    public class Email
+    {
+        public string NomeRemetente { get; set; }
+        public string Remetente { get; set; }
+        public string EnderecoRemetente { get; set; }
+
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+
+    }
+}
